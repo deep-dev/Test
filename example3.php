14bd49f8ddfc7c1700478d6b41fe09ef9464bc9c
deep-dev <deep.dev@yandex.ru>
2019-08-28 22:42:55 +0300

    Update example3.php some modification for better code and formatting.

--- example3.php
+++ example3.php
@@ -4,22 +4,30 @@ class File
 {
     private static $instances = array();
 	
-    private $handle;
-    private $strings = array();
-    private $count;
+    private $handle;                     // open file handle
+    private $strings = array();          // count of symbols in strings
+    private $count;                      // count of strings in file
 	
-    private function __construct($path)
+    public function __construct($path)
     {
         $this->count = 0;
         $this->handle = fopen($path, "r");
-        $this->definitionStrings();
+        
+		while (fgets($this->handle) !== false) {
+            $this->strings[] = ftell($this->handle);
+            ++$this->count;
+        }
     }
 	
-	public function __destruct()
-	{
-		$this->close();
-	}
+    public function __destruct()
+    {
+        $this->close();
+    }
 	
+    /**
+     * Represents a getInstance
+	 * @param string $path - file name for processed
+     */
     public static function getInstance($path)
     {
         if (!array_key_exists($path, self::$instances)) {
@@ -27,30 +35,27 @@ class File
         }
         return self::$instances[$path];
     }
-	
-    private function definitionStrings()
+
+	/**
+	 * Close file and reset to 0 all parameters
+	 */
+    public function close()
     {
-        while (fgets($this->handle) !== false) {
-            $this->strings[] = ftell($this->handle);
-            ++$this->count;
-        }
-    }
-	
-	public function close()
-	{
-		if ($this->handle) {
+        if ($this->handle) {
             flock($this->handle, LOCK_UN);
             fclose($this->handle);
             $this->handle = null;
             $this->count = 0;
-		}
-	}
+        }
+    }
 	
-	/* Check file descriptor return bool True if closed, false if is open */
-	public function isClosed()
-	{
-		return (bool) $this->file === null;
-	}
+    /**
+      * Check file descriptor return bool True if closed, false if is open
+	  */	  
+    public function isClosed()
+    {
+        return (bool) $this->file === null;
+    }
 	
     public function getCount() {
 		return ($this->isClosed()) ?  false : $this->count;
@@ -69,7 +74,7 @@ class File
     }
 }
 
-class FileIterator implements \SeekableIterator
+class FileIterator implements SeekableIterator
 {
     private $currentOffset;
     private $file;
@@ -85,49 +90,73 @@ class FileIterator implements \SeekableIterator
         }
         $this->currentOffset = $currentOffset;
     }
+	
+	/**
+	 * Rewind to start position in file
+	 */
     public function rewind()
     {
         $this->currentOffset = 0;
     }
+	
+	/**
+	 * Getting string with current position
+	 */
     public function current()
     {
         return $this->file->getString($this->currentOffset);
     }
 
-	/* return corrent offset */
+    /* return current offset */
     public function key()
     {
         return $this->currentOffset;
     }
 	
+	/**
+	 * Rewind to next position in file
+	 */
     public function next()
     {
         ++$this->currentOffset;
     }
-	
+
+	/**
+	 * Validate position in file
+	 */	
     public function valid()
     {
         return $this->currentOffset < $this->file->getCount();
     }
 }
 
-$myFile = 'example2.txt';
+$myFile = 'example3.txt';
 
-$fileIterator = new FileIterator( __DIR__ . '\\' . $myFile );
+try {
+    $fileIterator = new FileIterator( __DIR__ . '\\' . $myFile );
+    
+	$fileIterator->rewind();
+    $fileIterator->next();
+    $fileIterator->next();
+	
+	echo 'String: '. $fileIterator->key() ."<br>\n";
+	echo $fileIterator->current(), "<br>\n";
+	
+	
+    $fileIterator->seek(9);
+	echo 'String: '. $fileIterator->key() ."<br>\n";
+    echo $fileIterator->current(), "<br>\n";
 
-$fileIterator->rewind();
-$fileIterator->next();
-$fileIterator->next();
-$fileIterator->seek(9);
+    
+	echo 'Current string: '. $fileIterator->key() ."<br>\n";
+	echo $fileIterator->key();
 
-echo $fileIterator->key();
-
-//echo $fileIterator->current();
-
-foreach ($fileIterator as $char) {
-    echo $char;
-}
-
+    //echo $fileIterator->current();
+    /*
+    foreach ($fileIterator as $char) {
+        echo $char;
+    }
+    */
 /*
 $fileIterator->seek(3);
 $fileIterator->next();
@@ -141,6 +170,8 @@ $fileIterator->next();
 
 // $fileIterator->valid();
 
-
+} catch (OutOfBoundsException $e) {
+    echo $e->getMessage();
+}
 
 ?>
